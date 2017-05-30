ad097dd7fe0ee7e435db9caaa856a8afa2e72739
AJWroclaw <ak.italy@poczta.fm>
2017-05-30 20:37:27 +0200

    Tests for cantor

---
+++ LiderWalut.Tests1/LiderWalut.Tests1/MainPage.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+
+namespace LiderWalut.Tests
+{
+
+    public class MainPage
+    {
+        public readonly IWebDriver driver = new ChromeDriver();
+        public readonly string url = @"http://www.liderwalut.pl/";
+
+        public static By actionBuy = By.Id("action-buy");
+        public static By actionSell = By.Id("action-sell");
+        public static By actionChange = By.Id("action-change");
+        public static By destinationAmount = By.Id("destination_amount");
+
+        public void Teardown()
+        {
+            this.driver.Quit();
+        }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+
+        public void MaximizeWindow()
+        {
+            this.driver.Manage().Window.Maximize();
+        }
+
+        public void Wait()
+        {
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+
+        public List<string> GetTitles()
+        {
+            var titlesList = new List<string>();
+            var elements = this.driver.FindElements(By.CssSelector("div.title"));
+            foreach (IWebElement e in elements)
+            {
+                titlesList.Add(e.Text);
+            }
+            return titlesList;
+        }
+
+        public string GetDestinationAmount()
+        {
+            var destAmount = this.driver.FindElement(destinationAmount).Text;
+            return destAmount;
+        }
+
+        public void ClickButton(By action)
+        {
+            this.driver.FindElement(action).Click();
+        }
+    }
+}
---
+++ LiderWalut.Tests1/LiderWalut.Tests1/MainPageTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using LiderWalut.Tests;
+using System.Collections.Generic;
+
+namespace LiderWalut.Tests1
+{
+
+    [TestFixture]
+    public class MainPageTests
+    {
+        static MainPage browser = new MainPage();
+
+        [SetUp]
+        public void Initialize()
+        {
+
+            browser.Navigate();
+            browser.MaximizeWindow();
+            browser.Wait();
+        }
+
+        [Test]
+        public void CheckTransactionsLabelsAfterSwitchingTransactionTabs()
+        {
+            var listTitles = new List<string>();
+            var listExpected = new List<string>();
+
+            browser.ClickButton(MainPage.actionBuy);
+            listTitles = browser.GetTitles();
+            listExpected = new List<string>(new string[] { "WYSYŁASZ", "KURS WYMIANY", "KUPUJESZ" });
+            CollectionAssert.AreEqual(listTitles, listExpected, "Labels for buy method are not as expected");
+
+            browser.ClickButton(MainPage.actionSell);
+            listTitles = browser.GetTitles();
+            listExpected = new List<string>(new string[] { "SPRZEDAJESZ", "KURS WYMIANY", "OTRZYMUJESZ" });
+            CollectionAssert.AreEqual(listTitles, listExpected, "Labels for sell method are not as expected");
+
+            browser.ClickButton(MainPage.actionChange);
+            listTitles = browser.GetTitles();
+            listExpected = new List<string>(new string[] { "SPRZEDAJESZ", "KURS WYMIANY", "KUPUJESZ" });
+            CollectionAssert.AreEqual(listTitles, listExpected, "Labels for change method are not as expected");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            browser.Teardown();
+        }
+    }
+
+}
